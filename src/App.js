3cd89c82fbdff1be428f58d081e94a93fecc85a3
Andres Araya <araya.andres@gmail.com>
2019-07-11 07:01:13 -0600

    Add support for decimal values

--- src/App.js
+++ src/App.js
@@ -21,14 +21,21 @@ class App extends Component {
   state = {
     currentValue: 0,
     op: x => x,
+    decimalPosition: 0,
   }
 
   handleKey = key => {
-    const { currentValue, op } = this.state
+    const { currentValue, op, decimalPosition } = this.state
     let newValue = 0
     let newOp = x => x
+    let newDecimalPosition = 0
     if (/\d/.test(key)) {
-      newValue = currentValue * 10 + Number.parseInt(key)
+      if (decimalPosition === 0) {
+        newValue = currentValue * 10 + Number.parseInt(key)
+      } else {
+        newValue = currentValue + decimalPosition * Number.parseInt(key)
+        newDecimalPosition = decimalPosition * 0.1
+      }
       newOp = op
     } else if (/[+\-x/]/.test(key)) {
       const ops = {
@@ -41,10 +48,14 @@ class App extends Component {
       newOp = ops[key](value)
     } else if (key === '=') {
       newValue = op(currentValue)
+    } else if (key === '.' && decimalPosition === 0) {
+      newValue = currentValue
+      newDecimalPosition = .1
     }
     this.setState({
       currentValue: newValue,
       op: newOp,
+      decimalPosition: newDecimalPosition,
     })
   }
 
@@ -54,7 +65,7 @@ class App extends Component {
       [ '7', '8', '9', 'x' ],
       [ '4', '5', '6', '-' ],
       [ '1', '2', '3', '+' ],
-      [ '0', '=' ],
+      [ '0', '.', '=' ],
     ]
 
     return (
