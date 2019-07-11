0a310106e347793e834201d2e69b7e2874f4a5d6
Andres Araya <araya.andres@gmail.com>
2019-07-11 13:04:54 -0600

    Remove unused attribute

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -3,7 +3,7 @@ import Row from './Row'
 
 import './Keyboard.css'
 
-const keyboard = ({keys, click}) =>
+const keyboard = ({keys}) =>
   <div className='keyboard'>
     {keys.map((keys, i) => <Row key={i} keys={keys}/>)}
   </div>
