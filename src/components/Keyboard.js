4fc20004e4e1fba02f262740951e2e03c719a4df
Andres Araya <araya.andres@gmail.com>
2019-07-11 21:00:55 -0600

    Remove unused CSS files

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import Row from './Row'
 
-import './Keyboard.css'
-
 const keyboard = ({keys}) =>
   <div className='keyboard'>
     {keys.map((keys, i) => <Row key={i} keys={keys}/>)}
